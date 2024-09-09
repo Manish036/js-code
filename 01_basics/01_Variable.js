116831215745022427172a22f0698a058fcccd63
Manish036 <49864719+Manish036@users.noreply.github.com>
2024-09-09 12:32:09 +0000

    Let const Var

--- 01_basics/01_Variable.js
+++ 01_basics/01_Variable.js
@@ -1,7 +1,20 @@
 const account_Id = 444
 let accountEmail ="manish@microsoft.com"
 var accountPassword= "112233"
+accountcity = "Bihar"
+let accountState;
 
-account_Id =2
+//account_Id =2 not allowed
+
+
+accountEmail = "manish@hdfc.com"
+accountPassword= "2233"
+accountcity = "Bengaluru" // not good way
+
+
+// perefer not use varbecause of issue in block scope and functional scope
 
 console.log("account_Id");
+
+console.table ([account_Id,accountEmail,accountPassword,accountcity])
+
