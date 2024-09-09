cd448a623bd9b85d551cbc6f408e5af6f7b07ead
Manish036 <49864719+Manish036@users.noreply.github.com>
2024-09-09 13:18:04 +0000

    Data type

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,16 @@
+
+let name= manish
+let age = 6
+let isLoggedIn= false
+
+
+// Number
+// boolean = fasle/true
+//string= ""
+//null = type of value
+//undefined= not value
+//symbol 
+
+console.log(typeof age);
+console.log(typeof null);// undefined
+console.log(typeof undefined);// object
